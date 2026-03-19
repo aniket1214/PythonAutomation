62394c55a56ecac232e6d5f365d2b6b10b766e15
aniketshinde6618-byte <aniketshinde6618@gmail.com>
2026-03-19 19:38:08 +0530

    test

--- TestFile.py
+++ TestFile.py
@@ -1,1 +1,16 @@
-print("hi")
+
+# 14). Write a python to repeat vowels 3 times and consonants 2 times.
+# Input = “Sqa Tools Learning”
+# Ouput = “SSqqaaa TToooooollss LLeeeaaarrnniiinngg”
+
+
+input = "Sqa Tools Learning"
+vowel = "aeiouAEIOU"
+output=""
+for word in input:
+    if word in vowel:
+        output = output + (word*3)
+    else:
+        output = output + (word*2)
+
+print(output)
